ac4f495a34ca124abcdf0afcbf826006dc5e9be7
Bogeunk <95890510+Bogeunk@users.noreply.github.com>
2022-02-14 17:02:30 +0900

    make 'join' function mypage.html login.html mysql connect

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
-from .views import Join
+from .views import Join, Login, Mypage
 from django.urls import path
 
 urlpatterns = [
     path('join/', Join.as_view()),
+    path('login/', Login.as_view()),
+    path('mypage/', Mypage.as_view())
 ]

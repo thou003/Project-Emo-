22286229dd2657c02fc96bf722a5ef142366d4db
Bogeunk <95890510+Bogeunk@users.noreply.github.com>
2022-02-17 11:26:37 +0900

    0217 update

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,16 @@
+from . import views
 from .views import Join, Login, Mypage, Edituser, Updateuser
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
+app_name = 'user'
 
 urlpatterns = [
     path('join/', Join.as_view()),
-    path('login/', Login.as_view()),
+    path('login/', Login.as_view(), name='login'),
     path('mypage/', Mypage.as_view()),
     path('edituser/', Edituser.as_view()),
-    path('update/', Updateuser.as_view())
+    path('update/', Updateuser.as_view()),
+    path('quit/', views.deleteuser, name='quitmember'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
